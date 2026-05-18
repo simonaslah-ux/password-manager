06a5884099cce662ae00004f9439698d497b0183
Simona Slah <simona.slah@stud.svako.lt>
2026-05-18 11:56:12 +0300

    Pradine projekto struktura

---
+++ README.md
@@ -0,0 +1,11 @@
+# Slaptažodžių generatorius ir saugykla
+
+Tai PHP OOP pagrindu kuriamas projektas, skirtas slaptažodžių generavimui ir saugojimui.
+
+## Planuojamas funkcionalumas
+
+- Vartotojų registracija
+- Prisijungimas prie sistemos
+- Slaptažodžių generavimas pagal pasirinktus parametrus
+- Slaptažodžių saugojimas MySQL duomenų bazėje
+- Slaptažodžių šifravimas AES metodu
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="UTF-8">
+    <title>Slaptažodžių generatorius ir saugykla</title>
+</head>
+<body>
+
+    <h1>Slaptažodžių generatorius ir saugykla</h1>
+    <p>PHP OOP pagrindu kuriama programa slaptažodžių generavimui ir saugojimui.</p>
+
+    <a href="views/register.php">Registruotis</a>
+    <a href="views/login.php">Prisijungti</a>
+
+</body>
+</html>
